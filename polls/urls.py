72a437e5590b7294f4de1ff8dfd57ffb6c0099db
sako16 <sako.alizada.06@gmail.com>
2025-10-26 18:29:00 +0400

    .
    .

--- polls/urls.py
+++ polls/urls.py
@@ -1,4 +1,4 @@
-# polls/urls.py
+# polls/urls.py (Düzəldilmiş versiya)
 from django.urls import path
 from . import views
 
@@ -7,12 +7,15 @@ urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'), 
     
-    # ex: /polls/5/ <--- YENİ ƏLAVƏ ETMƏLİ OLDUĞUNUZ SATIR
+    # ex: /polls/5/
     path('<int:question_id>/', views.detail, name='detail'), 
     
-    # ex: /polls/5/results/
-    # path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/results/ <-- Şərhi yığışdırdıq
+    path('<int:question_id>/results/', views.results, name='results'),
     
-    # ex: /polls/5/vote/
-    # path('<int:question_id>/vote/', views.vote, name='vote'),
+    # ex: /polls/5/vote/ <-- Şərhi yığışdırdıq VƏ listin içərisinə köçürdük
+    path('<int:question_id>/vote/', views.vote, name='vote'), 
 ]
+
+# AŞAĞIDAKİ SƏTRİ SİLİN: Çünki yuxarıda artıq listin daxilindədir.
+# path("<int:question_id>/vote/", views.vote, name="vote"),
--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,19 @@
 # polls/views.py
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse # index üçün lazımdır
-from .models import Question
 
-# TƏLƏB OLUNAN DÜZƏLİŞ: index funksiyasını da şablon istifadə edəcək şəkildə dəyişin
+# Çatışmayan funksiyaları və modelləri import edirik
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse # HttpResponseRedirect və HttpResponse-u əlavə etdik
+from django.urls import reverse # reverse funksiyasını əlavə etdik
+from django.db.models import F # F ifadəsini əlavə etdik
+
+# Question modelini import etmişdik, Choice modelini də əlavə edirik (Çünki vote funksiyasında istifadə olunur)
+from .models import Question, Choice 
+
+
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     
-    # OUTPUT-u (çıxışı) sadə mətn əvəzinə, şablon vasitəsilə göndərin
+    # OUTPUT-u (çıxışı) sadə mətn əvəzinə, şablon vasitəsilə göndəririk
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
     
@@ -17,9 +23,28 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    # Bu funksiyalar hələlik belə qala bilər, amma təkrar yazılmamalıdır.
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    # Düzəliş: Sondakı əlavə mötərizəni sildik.
+    return render(request, "polls/results.html", {"question": question}) 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        # Seçim id-si (pk) POST data vasitəsilə gəlir
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Səs verilmədiyi halda xəta mesajı ilə 'detail' şablonuna qayıdırıq.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        # F("votes") + 1 atomar bir əməliyyat üçün istifadə olunur
+        selected_choice.votes = F("votes") + 1 
+        selected_choice.save()
+        # POST datası uğurla idarə edildikdən sonra həmişə HttpResponseRedirect qaytarın.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
